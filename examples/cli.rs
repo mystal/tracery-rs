3b131c4b663534223fb8f2d97dcece1c096fab7d
Gabriel Martinez <reitaka@gmail.com>
2023-07-19 22:28:56 -0400

    cli: Add option to generate multiple lines

--- examples/cli.rs
+++ examples/cli.rs
@@ -2,11 +2,30 @@ use std::fs;
 
 use tracery::{self, Result};
 
+const USAGE: &str = r#"
+Usage: cli <JSON_FILE> [N]
+
+Arguments:
+  <JSON_FILE> Path to Tracery JSON file.
+  [N]         Number of lines to generate (default: 1).
+"#;
+
 fn main() -> Result<()> {
-    if let Some(json_path) = std::env::args_os().nth(1) {
-        let json_str = fs::read_to_string(json_path).unwrap();
-        eprintln!("{}", json_str);
-        println!("{}", tracery::flatten_json(json_str)?);
+    let mut args = std::env::args_os().skip(1);
+
+    // Parse args.
+    let Some(json_path) = args.next() else {
+        println!("{}", USAGE.trim());
+        return Ok(());
+    };
+    let n: u32 = args.next()
+        .and_then(|s| s.into_string().ok())
+        .and_then(|s| s.parse().ok())
+        .unwrap_or(1);
+
+    let json_str = fs::read_to_string(json_path).unwrap();
+    for _ in 0..n {
+        println!("{}", tracery::flatten_json(&json_str)?);
     }
 
     Ok(())
